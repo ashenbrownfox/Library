7d88ec158d74cceb5a2cb367f9cdc49c4b447225
ashenbrownfox <newhoopsdynasty@gmail.com>
2018-11-20 15:03:56 -0500

    Added LibraryData Models

---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public Patron()
+        {
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        //public virtual LibraryCard LibraryCard { get; set; }
+        //use virtual keyword when you want to lazy load that property's data
+        //lazy loading loada collection from the db the first time it is accessed
+    }
+}
